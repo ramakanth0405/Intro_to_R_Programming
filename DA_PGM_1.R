63e2a928478bb16ef74b5674a952734895130413
Ramakanth <ramakanthjrv@gmail.com>
2025-12-05 10:56:42 +0530

    Added Relational Operators and Logical Opertators

--- DA_PGM_1.R
+++ DA_PGM_1.R
@@ -1,3 +1,17 @@
 x <- 10
 print('Hello World',x)
 
+10/3 # Returns Quotient
+10%%3 # Returns Remainder
+10%/%3 # Return Integer Quotient (Rounding off)
+
+# Relational Operation
+10>3 # Returns Boolean
+10!=3 
+
+TRUE || TRUE # AND OPEARTOR ABBREVIATION ||
+
+TRUE | TRUE # OR OPERATOR ABBREVIATION |
+
+c(189191991,0) & c(40,0) # Non Zero Numbers are true 
+40 && 0 # sibgle & Compares a single number
