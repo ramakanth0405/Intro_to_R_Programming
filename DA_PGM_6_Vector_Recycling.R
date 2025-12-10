ef8ba54734edf0c9880d48b41c1baeec87fe24bf
Ramakanth <ramakanthjrv@gmail.com>
2025-12-10 10:24:12 +0530

    Program 6 for Vector Recycling. Demonstration of Perfect Recycling of shorter array when the longer array is a multiple of the shorter array.

---
+++ DA_PGM_6_Vector_Recycling.R
@@ -0,0 +1,8 @@
+# Vector Recycling
+
+# Perfect Recycling
+# If the longer array is an exact multiple of the shorter array
+# the array repeats
+x <- c(1,2,3,4)
+y <- c(1,2)
+x+y # O/P is 2,4,4,6 : The element (1,2) repeats (1,2,1,2)
