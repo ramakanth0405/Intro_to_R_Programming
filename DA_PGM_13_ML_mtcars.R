357b415f3f465ab7f748abc86509a573687afb90
ramakanth0405 <ramakanthjrv@gmail.com>
2026-01-20 12:38:26 +0530

    Modified DA_PGM_13_ML_mtcars.R, changed model variable from fit to model, added R Code for Residual vs Fitted Plot, Density Plot, Residual vs Leverage Plot. Need to add R Code for Quantile-Quantile Plot

--- DA_PGM_13_ML_mtcars.R
+++ DA_PGM_13_ML_mtcars.R
@@ -11,13 +11,13 @@ d <- mtcars
 
 # Fitting the model in Linear Regression
 # We are going to analyse how the fuel efficiency (mpg) varies with weight (wt)
-fit <- lm(mpg ~ wt, data = d)
+model <- lm(mpg ~ wt, data = d)
 
 # Predict the values using linear regression model
-d$predicted <- predict(fit)
+d$predicted <- predict(model)
 
 # Calculate the residuals from the fitted model
-d$residuals <- residuals(fit)
+d$residuals <- residuals(model)
 
 ggplot(d, aes(x=wt, y= mpg)) +
   geom_smooth(method='lm', se=FALSE, color='lightgrey') + # Regression Line
@@ -35,3 +35,23 @@ ggplot(d, aes(x=wt, y= mpg)) +
   geom_point(aes(y= predicted), shape =1) +
   
   theme_bw()
+
+# Plot: RESIDUAL vs FITTED PLOT
+res <- resid(model)
+plot(fitted(model), res)
+abline(0,0)
+
+# Plot: Density Plot
+# Use: To check normality of residuals
+plot(density(res))
+
+
+# Plot: Residual vs Leverage Plot
+plot(model, which=5, col=c('blue'))
+# The line to the right top or bottom left is known as Cook's Distance(Grey Dotted Line),
+# The points above the Cook's Distance are mostly influential points/ outliers
+
+# Plot: Quantile - Quantile Plot (Q-Q Plot)
+# Use: Mostly for checking Normality of residuals
+# Additionally Q-Q Plot can also be used for checking whether two distributions are similar
+
