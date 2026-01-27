743cc7811be24d8e1120d48b4b4712c7a0c400dd
ramakanth0405 <ramakanthjrv@gmail.com>
2026-01-27 11:56:26 +0530

    Modified DA_PGM_14T_Decision_Tee.R and added code for building Decision Tree, Visualize Decision Tree. Need to add more documentation

--- DA_PGM_14T_Decision_Tree.R
+++ DA_PGM_14T_Decision_Tree.R
@@ -1,3 +1,4 @@
+# ML Model Decision Tree to classify iris flowers into species (setosa, versicolor, virginica)
 library(rpart)
 library(rpart.plot)
 library(caret)
@@ -5,5 +6,18 @@ library(caret)
 data(iris)
 set.seed(123)
 train_index <- createDataPartition(iris$Species, p=0.8, list=FALSE)
+# 80 Percent Training Set, 20 Percent Test Data
+# list = FALSE, to appear not as list
 train_data <- iris[train_index,]
 test_data <- iris[-train_index,]
+
+# Build Decision Tree
+tree_model <- rpart(Species~.,
+                    data = train_data,
+                    method = 'class',
+                    control = rpart.control(minsplit = 10, cp = 0.01)) # For Classification
+
+# Visualizing Decision Tree
+rpart.plot(tree_model, box.palette='auto', nn=TRUE)
+# box.palette: Used to provide color to nodes
+# nn : node number
