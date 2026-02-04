8044849fc7383ac4dac2f36355ab2d7a9e8bc24a
ramakanth0405 <ramakanthjrv@gmail.com>
2026-02-04 10:28:25 +0530

    Modified DA_PGM_17T_Text_Mining.R by adding code for stemming, sentiment analysis, wordcloud

--- DA_PGM_17T_Text_Mining.R
+++ DA_PGM_17T_Text_Mining.R
@@ -27,3 +27,33 @@ clean_tokens <- tokens %>%
 
 # View cleaned tokens
 print(clean_tokens)
+
+# Calculate word frequency
+word_frequency <- clean_tokens %>% 
+  count(word, sort = TRUE)
+
+# View word frequency
+print(word_frequency)
+
+
+library(SnowballC)
+# SnowballC is a library for stemming words
+stemmed_tokens <- clean_tokens %>% 
+  mutate(stem = wordStem(word))
+
+print(stemmed_tokens)
+
+# Sentiment Analysis_Lexicon
+sentiment_lexicon <- get_sentiments('bing')
+
+# Perform sentiment analysis
+sentiment_analysis <- clean_tokens %>%
+  inner_join(sentiment_lexicon, by = 'word') %>% 
+  count(word, sentiment, sort = TRUE)
+
+print(sentiment_analysis)
+
+library(wordcloud)
+
+wordcloud(words = word_frequency$word, freq = word_frequency$n, min.freq = 1,
+          random.order = FALSE, color = brewer.pal(8, 'Dark2'))
